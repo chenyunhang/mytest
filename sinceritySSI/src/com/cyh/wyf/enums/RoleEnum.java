8b50bb78b1965bde98504be882a622f6840b2a4f
chenyh <1154215990@qq.com>
2018-07-16 23:17:23 +0800

    提交

--- sinceritySSI/src/com/cyh/wyf/enums/RoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.cyh.wyf.enums;
-
-public interface RoleEnum {	
-	public int ADMIN = 1;
-	public int CUSER = 2;
-}
--- sinceritySSI/src/com/cyh/wyf/util/Log.java
+++
@@ -1,9 +0,0 @@
-package com.cyh.wyf.util;
-
-import org.apache.log4j.Logger;
-
-public class Log {
-	
-	public static Logger logger =  Logger.getLogger(Log.class.getName());
-
-}
